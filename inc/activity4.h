e6f3a17aabbc472a559c32795d288523dd7281fb
256644 <ramya.1si17ec077@gmail.com>
2021-04-29 12:18:38 +0530

    updates

---
+++ inc/activity4.h
@@ -0,0 +1,5 @@
+#ifndef __ACTIVITY_H__
+#define __ACTIVITY_H__
+void USART_Init(unsigned int ubrr);
+void USART_Transmit(unsigned char data);
+#endif
--- src/activity1.c
+++ src/activity1.c
@@ -7,6 +7,7 @@ uint8_t check_seating()
   if((!(PIND&(1<<PD0))) &&(!(PIND&(1<<PD1))))
 {
 
+
     return 1;
 }
 return 0;
