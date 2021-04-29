608e1d65720a7d762f699a5a21ea7274408b43e0
256644 <ramya.1si17ec077@gmail.com>
2021-04-30 00:29:41 +0530

    changes

---
+++ documentation/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['init_5fperipherial_0',['init_peripherial',['../main_8c.html#a8f194b8bb531e753937f2e594e941a7b',1,'main.c']]]
+];
---
+++ documentation/html/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['init_5fperipherial_6',['init_peripherial',['../main_8c.html#a8f194b8bb531e753937f2e594e941a7b',1,'main.c']]]
+];
